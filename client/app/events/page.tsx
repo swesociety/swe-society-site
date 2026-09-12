5a60af3a777c55ad0214273edb7a5704d65bc880
Md. Mustakin Ebne Mazid <mustakin.mazid@brainstation-23.com>
2026-09-12 17:12:21 +0600

    fix dynamic rendering issue

--- client/app/events/page.tsx
+++ client/app/events/page.tsx
@@ -1,15 +1,12 @@
-// import EventSection from "@/components/homepage/EventSection";
-// export default function page() {
-//   return <div>
-
 import { PaginatedEvents } from "@/components/eventspage/eventsection";
 import { BACKENDURL } from "@/data/urls";
 
+export const dynamic = "force-dynamic";
 
 async function fetchEvents() {
   try {
     const res = await fetch(`${BACKENDURL}event/`, { 
-      cache: 'no-store' // or 'force-cache' depending on your needs
+      cache: 'no-store'
     });
     return res.json();
   } catch (error) {
